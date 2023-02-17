22213d1988f42c8a548dd3feb69c0e52fcaea204
Ondrej Bruha <ondrej.bruha@outlook.com>
2023-02-17 22:09:04 +0100

    nepotrebne soubory

--- src/dataStore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import {dataSlice} from "./dataSlice";
-
-const store =  configureStore({
-    reducer: {
-        data: dataSlice.reducer
-    }
-});
-export default store;
