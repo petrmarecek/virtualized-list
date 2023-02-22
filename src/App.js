2adc0b29ac3c1f2821462985c8e63d3f8a75d43d
Ondrej Bruha <ondrej.bruha@outlook.com>
2023-02-22 07:31:33 +0100

    oprava

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from "react";
 
 import {useVirtualizer} from "@tanstack/react-virtual";
@@ -15,18 +14,21 @@ function dataReducer(state = {value: []}, action) {
 }
 
 const MainDiv = styled.div`
-  height: 100vh;
-  width: 100vw;
+  height: 200px;
+  width: 400px;
+  overflow: auto;
 `;
 const Container = styled.div`
-  height: ${(props)=>{return props.height}};
+  height: ${props => props.height};
   width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
 `;
 const Row = styled.div`
+  height: ${props => props.height};
+  transform: translateY(${props => props.start});
   text-align: center;
   padding: 1rem 6rem;
 
@@ -35,28 +37,31 @@ const Row = styled.div`
   }
 `;
 
-
+const count = 10000;
+let store = createStore(dataReducer);
+for (let i = 0; i < count; i++) {
+    store.dispatch({type: "add"})
+}
 function App() {
-    const count = 10000;
-    let store = createStore(dataReducer);
-    for (let i = 0; i < count; i++) {
-        store.dispatch({type: "add"})
-    }
+
     const parentRef = React.useRef();
     const rowVirtualizer = useVirtualizer({
         count: count,
         getScrollElement: () => parentRef.current,
-        estimateSize: () => 50,
+        estimateSize: () => 35,
+        overscan: 5,
     });
 
     return (
-        <MainDiv className="App" style={{}}>
-            <Container className={"container"} ref={parentRef} height={`${rowVirtualizer.getTotalSize()}px`}>
-                {rowVirtualizer.getVirtualItems().map((itm) => (
-                    <Row>{store.getState().value[itm.index].subject}</Row>
-                ))}
-            </Container>
-        </MainDiv>
+        <>
+            <MainDiv className="App" ref={parentRef}>
+                <Container className={"container"} height={`${rowVirtualizer.getTotalSize()}px`}>
+                    {rowVirtualizer.getVirtualItems().map((itm) => (
+                        <Row height={`${itm.size}px`} start={`${itm.start}px`}>{store.getState().value[itm.index].subject}</Row>
+                    ))}
+                </Container>
+            </MainDiv>
+        </>
     );
 }
 
